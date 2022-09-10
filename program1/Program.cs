0199993cf931c9db5a4f453136fbdccc4da480b2
Taya <tayakolodeeva@yandex.ru>
2022-09-10 21:56:41 +0300

    fixprogram1

---
+++ program1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Решить задачу с использованием методов
+int DegreeOfNumber (int num1, int num2)
+
+{
+int degree = num1;  
+ for (int i = 1; i < num2; i++ )
+{
+degree = (degree * num1);
+}
+{
+   return degree;
+}
+}
+
+
+Console.WriteLine("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число (степень):");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int degreenum = DegreeOfNumber (num1, num2);
+Console.WriteLine($"Число {num1} в степени {num2} = {degreenum}");
