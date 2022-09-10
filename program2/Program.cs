5d35c94e447f0756fb4dc30f6b877079542dc3ba
Taya <tayakolodeeva@yandex.ru>
2022-09-11 00:29:50 +0300

    fixprogram3

---
+++ program2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumOfDigits (int num)
+{
+int quantity = Convert.ToString(num).Length;
+int number = 0;
+int sum = 0;
+for (int i = 0; i < quantity; i++)
+{
+number = num - num % 10;
+sum = sum + (num - number);
+num = num / 10;
+}
+{
+return sum;
+}
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumOfDigits (num);
+Console.WriteLine($" Сумма цыфер числа {num} = {sum}");
---
+++ program3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы массива вводятся пользователем.
+
+            int[] array = new int[8];
+    
+ 
+            Console.WriteLine("Введите 8 элементов массива (элемент -> enter):");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+            Console.WriteLine("Созданный массив");
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                Console.Write($"{array[i]} ");
+            }
+            
